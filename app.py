73bfb1978239e11250b7920853db4e6fb005684b
Manar57 <159260551+Manar57@users.noreply.github.com>
2026-05-08 17:09:16 +0300

    update project

--- app.py
+++ app.py
@@ -294,5 +294,8 @@ def health():
     return jsonify({'status': 'ok', 'model_loaded': True})
 
 
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
